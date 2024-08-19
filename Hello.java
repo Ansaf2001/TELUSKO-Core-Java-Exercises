44dffc6c7979dbb7cdee7401ce07f8caed386842
ansaf <ansafansar2001@gmail.com>
2024-08-19 11:15:02 +0530

    Add Hello java File

--- Hello.java
+++ Hello.java
@@ -1,7 +1,9 @@
 class Hello{
     public static void main(String args[])
     {
-        System.out.println("Hello World!");
-        System.out.println(4+8);
+        
+        // System.out.println("Hello World!");
+        // System.out.println(4+8);
+        // System.out.println(5+8);
     }
 }
